600849b58c80c25f3810b7d481922868b3927a3b
wendymackennah <wendymackennah5@gmail.com>
2024-01-15 13:08:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+const reversedWord= reverse(word);
+return word === reversedWord;
+
+
+}
+function reverse(word){
+  return word.split('').reverse().join("");
 }
 
-/* 
-  Add your pseudocode here
-*/
 
-/*
-  Add written explanation of your solution here
-*/
+  //Add your pseudocode here
+
+
+//Add written explanation of your solution here
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
